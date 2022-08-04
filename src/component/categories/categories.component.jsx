ef0863101e498a2aa2522d940e35fa4d260def1e
Hardy Milalu <hmilalu@talend.com>
2022-08-04 15:31:04 +0200

    updating our categories page with categories component and category item component

---
+++ src/component/categories/categories.component.jsx
@@ -0,0 +1,18 @@
+import './categories.style.scss'
+import CategoryItem from '../category-item/category-item.component';
+
+const Categories = (props) => {
+
+    const {categories} = props;
+
+    return(
+        <div className="categories-container">
+        {categories.map((category) => (
+          <CategoryItem key={category.id} category = {category}/>
+        ))} 
+      </div>
+    )
+      
+}
+
+export default Categories; 
